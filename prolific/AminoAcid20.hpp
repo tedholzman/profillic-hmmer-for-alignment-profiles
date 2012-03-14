cef239182f5fba2a5d923b35fa0899a51c020c45
Ted Holzman <tholzman@scharp.org>
2012-03-13 19:59:09 -0700

    Initial commit of files to profillic-hmmer for alignment profiles

---
+++ prolific/AminoAcid20.hpp
@@ -0,0 +1,114 @@
+/*---------------------------------------------------------------------------##
+##  Library:
+##      galosh::prolific
+##  File:
+##      AminoAcid20.hpp
+##  Author:
+##      D'Oleris Paul Thatcher Edlefsen   paul@galosh.org
+##  Description:
+##      A 20-char alternative to seqan::AminoAcid (which includes a 21st char for
+##      the stop codon).
+##
+#******************************************************************************
+#*
+#*    This file is part of prolific, a library of useful C++ classes for
+#*    working with genomic sequence data and Profile HMMs.  Please see the
+#*    document CITING, which should have been included with this file.  You may
+#*    use at will, subject to the license (Apache v2.0), but *please cite the
+#*    relevant papers* in your documentation and publications associated with
+#*    uses of this library.  Thank you!
+#*
+#*    Copyright (C) 2011 by Paul T. Edlefsen, Fred Hutchinson Cancer
+#*    Research Center.
+#*
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *    
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *    
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+#*****************************************************************************/
+
+#if     _MSC_VER > 1000
+#pragma once
+#endif
+
+#ifndef __GALOSH_AMINOACID20_HPP__
+#define __GALOSH_AMINOACID20_HPP__
+
+#include <seqan/basic.h>
+
+#include "Prolific.hpp"
+
+namespace seqan {
+
+// Copied and modified from SeqAn (GNU Lesser General Public License v3) : $Id: basic_alphabet_simple.h 4412 2009-06-08 16:21:28Z rausch@PCPOOL.MI.FU-BERLIN.DE $
+
+//____________________________________________________________________________
+
+/**
+.Spec.AminoAcid20:
+..cat:Alphabets
+..summary:Iupac code for amino acids.
+..general:Class.SimpleType
+..signature:AminoAcid20
+..remarks:
+...text:The @Metafunction.ValueSize@ of $AminoAcid20$ is 20. 
+...text:The amino acids are enumerated from 0 to 19 in this order: 
+...text:'A'=0, 'R', 'N', 'D', 'C', 'Q', 'E', 'G', 'H', 'I', 'L', 'K', 'M', 'F', 'P', 'S', 'T', 'W', 'Y', 'V'=19.
+...text:Objects of type $AminoAcid20$ can be converted to $char$ and vice versa. 
+Unkown values are converted to $'A'$.
+...text:$AminoAcid20$ is typedef for $SimpleType<char,_AminoAcid20>$, while $_AminoAcid20$ is a helper
+specialization tag class.
+..see:Metafunction.ValueSize
+*/
+struct _AminoAcid20 {};
+typedef SimpleType<unsigned char, _AminoAcid20> AminoAcid20;
+
+template <> struct ValueSize< AminoAcid20 > { enum { VALUE = 20 }; };
+template <> struct BitsPerValue< AminoAcid20 > { enum { VALUE = 5 }; };
+
+inline void assign(Ascii & c_target, AminoAcid20 const & source)
+{
+SEQAN_CHECKPOINT
+	c_target = _Translate_Table_AA_2_Ascii<>::VALUE[source.value];
+}
+
+//////////////////////////////////////////////////////////////////////////////
+//Amino Acid (5 bits)
+
+template <>
+struct CompareType<AminoAcid20, Byte> { typedef AminoAcid20 Type; };
+inline void assign(AminoAcid20 & target, Byte c_source)
+{
+SEQAN_CHECKPOINT
+	target.value = _Translate_Table_Byte_2_AA<>::VALUE[c_source];
+}
+//____________________________________________________________________________
+
+template <>
+struct CompareType<AminoAcid20, Ascii> { typedef AminoAcid20 Type; };
+inline void assign(AminoAcid20 & target, Ascii c_source)
+{
+SEQAN_CHECKPOINT
+	target.value = _Translate_Table_Ascii_2_AA<>::VALUE[(unsigned char) c_source];
+}
+//____________________________________________________________________________
+
+template <>
+struct CompareType<AminoAcid20, Unicode> { typedef AminoAcid20 Type; };
+inline void assign(AminoAcid20 & target, Unicode c_source)
+{
+SEQAN_CHECKPOINT
+	target.value = _Translate_Table_Ascii_2_AA<>::VALUE[(unsigned char) c_source];
+}
+
+//////////////////////////////////////////////////////////////////////////////
+} // End namespace seqan
+
+#endif // __GALOSH_AMINOACID20_HPP__
---
+++ prolific/Prolific.hpp
@@ -0,0 +1,95 @@
+/*---------------------------------------------------------------------------##
+##  Library:
+##      galosh::prolific
+##  File:
+##      Prolific.hpp
+##  Author:
+##      D'Oleris Paul Thatcher Edlefsen   paul@galosh.org
+##  Description:
+##      Global #defines for Prolific.
+##
+#******************************************************************************
+#*
+#*    This file is part of prolific, a library of useful C++ classes for
+#*    working with genomic sequence data and Profile HMMs.  Please see the
+#*    document CITING, which should have been included with this file.  You may
+#*    use at will, subject to the license (Apache v2.0), but *please cite the
+#*    relevant papers* in your documentation and publications associated with
+#*    uses of this library.  Thank you!
+#*
+#*    Copyright (C) 2008, 2011 by Paul T. Edlefsen, Fred Hutchinson Cancer
+#*    Research Center.
+#*
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *    
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *    
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+#*****************************************************************************/
+
+#if     _MSC_VER > 1000
+#pragma once
+#endif
+
+#ifndef __GALOSH_PROLIFIC_HPP__
+#define __GALOSH_PROLIFIC_HPP__
+
+#if defined __MSC_VER
+#include <boost/math/special_functions/log1p.hpp>
+using boost::math::log1p;
+#endif
+
+////// What shall we inline?
+#define GALOSH_INLINE_INIT inline
+#define GALOSH_INLINE_REINITIALIZE inline
+#define GALOSH_INLINE_COPY inline
+#define GALOSH_INLINE_ACCESSOR inline
+#define GALOSH_INLINE_OSTREAM
+
+// "trivial" means it is just a renaming of another fn (so all it does is call that other fn), or perhaps returning simple function of that call.
+#define GALOSH_INLINE_TRIVIAL inline
+
+// Probabilities
+#ifdef GALOSH_USE_ICSILOG
+  #define GALOSH_LOG(x) icsilogv2(x)
+#else
+  #define GALOSH_LOG(x) ::log(x)
+#endif
+#if defined __MSC_VER
+#define GALOSH_LOG1P(x) boost::math::log1p(x)
+#else
+#define GALOSH_LOG1P(x) ::log1p(x)
+#endif
+
+// MultinomialDistribution
+#define GALOSH_INLINE_MULTINOMIALDISTRIBUTION_ARITHMETIC inline
+#define GALOSH_INLINE_MULTINOMIALDISTRIBUTION_COMPARE inline
+#define GALOSH_INLINE_MULTINOMIALDISTRIBUTION_NORMALIZE_ENSUREMIN inline
+#define GALOSH_INLINE_MULTINOMIALDISTRIBUTION_COMPLEX_ACCESSOR inline
+#define GALOSH_INLINE_MULTINOMIALDISTRIBUTION_EUCLIDEANDISTSQ inline
+#define GALOSH_INLINE_MULTINOMIALDISTRIBUTION_DRAW inline
+#define GALOSH_INLINE_MULTINOMIALDISTRIBUTION_FROMDIRICHLET inline
+#define GALOSH_INLINE_MULTINOMIALDISTRIBUTION_TO_BOLTZMANNGIBBS inline
+#define GALOSH_INLINE_MULTINOMIALDISTRIBUTION_FROM_BOLTZMANNGIBBS inline
+
+// Parameters, DynamicProgramming, ...
+#define GALOSH_INLINE_PARAMETERSMODIFIER_APPLY_MODIFICATIONS inline
+
+// Profile
+#define GALOSH_INLINE_PROFILE_ARITHMETIC inline
+#define GALOSH_INLINE_PROFILE_EUCLIDEANDISTSQ inline
+#define GALOSH_INLINE_PROFILE_CROSSENTROPY inline
+#define GALOSH_INLINE_PROFILE_FREEPARAMCOUNT inline
+#define GALOSH_INLINE_PROFILE_COMPLEX_ACCESSOR inline
+
+// DynamicProgramming
+#define GALOSH_INLINE_ALGORITHM inline
+#define GALOSH_INLINE_ALGORITHM_INNERLOOP inline
+
+#endif // __GALOSH_PROLIFIC_HPP__
